e048aa0fba0ba6eb9eabc0f705fc3d271b87c1bd
Dracologist <ejckanzler@yahoo.com>
2018-01-18 17:07:33 -0500

    added images

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
 
     $f3->route('GET /show/cat', function(){
 
-    echo '<h1>Cat</h1>';
+    echo '<img src="https://s7d1.scene7.com/is/image/PETCO/cat-category-090617-369w-269h-hero-cutout-d?fmt=png-alpha">';
     });
 
     $f3->route('GET /show/dog', function(){
 
-    echo '<h1>Dog</h1>';
+    echo '<img src="https://s7d1.scene7.com/is/image/PETCO/dog-category-090617-369w-269h-hero-cutout-d?fmt=png-alpha">';
     });
 
     $f3->run();
