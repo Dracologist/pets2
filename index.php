2b45843b9a84be453fd0d2ae81f27cc76cc9c67c
Shahbaz Iqbal <siqbal2@mail.greenriver.edu>
2018-01-18 17:33:01 -0500

    updated views and index

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     $f3 = Base::instance();
 
     $f3->route('GET /', function() {
-        $view = new View;    echo $view->render
-        ('views/home.html');
+        $view = new View;
+        echo $view->render('views/home.html');
     });
 
     $f3->route('GET /show/cat', function(){
@@ -19,14 +19,14 @@
     });
 
     $f3->route('GET /order', function() {
-
-        echo '<h1>Form 1</h1>';
+        $view = new View;
+        echo $view->render('views/form1.html');
     });
-    $f3->route('GET /order2', function() {
-
-        echo '<h1>Form 2</h1>';
+    $f3->route('POST /order2', function() {
+        $view = new View;
+        echo $view->render('views/form2.html');
     });
-    $f3->route('GET /results', function() {
+    $f3->route('POST /results', function() {
 
         echo '<h1>Results</h1>';
     });
