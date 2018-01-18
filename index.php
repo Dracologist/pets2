2a2cdac88d7848d2d4611213fa7c30b35c6f37c2
Dracologist <ejckanzler@yahoo.com>
2018-01-18 16:53:33 -0500

    Added default path

--- index.php
+++ index.php
@@ -2,13 +2,9 @@
     require_once('vendor/autoload.php');
     $f3 = Base::instance();
 
-//    $f3->route('GET /', function() {
-//        echo '<h1>Hello, world!</h1>';
-//    });
-
-//    $f3->route('GET /', function() {
-//        $view = new View;    echo $view->render
-//        ('views/home.html');
-//    });
+    $f3->route('POST /', function() {
+        $view = new View;    echo $view->render
+        ('views/home.html');
+    });
 
     $f3->run();
