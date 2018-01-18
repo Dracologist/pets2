718f35bb6d9bb5a2e0815bd0325635094bcc69b4
Shahbaz Iqbal <siqbal2@mail.greenriver.edu>
2018-01-18 17:02:49 -0500

    added animals

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
     require_once('vendor/autoload.php');
     $f3 = Base::instance();
 
-    $f3->route('POST /', function() {
+    $f3->route('GET /', function() {
         $view = new View;    echo $view->render
         ('views/home.html');
+    });
+
+    $f3->route('GET /show/cat', function(){
+
+    echo '<h1>Cat</h1>';
+    });
+
+    $f3->route('GET /show/dog', function(){
+
+    echo '<h1>Dog</h1>';
     });
 
     $f3->run();
