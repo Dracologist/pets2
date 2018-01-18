f3b8a3186c8da5ad770b25c80e98813f8c43070a
Dracologist <ejckanzler@yahoo.com>
2018-01-18 17:20:39 -0500

    Created routes for orders

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     require_once('vendor/autoload.php');
     $f3 = Base::instance();
 
@@ -15,6 +16,19 @@
     $f3->route('GET /show/dog', function(){
 
     echo '<img src="https://s7d1.scene7.com/is/image/PETCO/dog-category-090617-369w-269h-hero-cutout-d?fmt=png-alpha">';
+    });
+
+    $f3->route('GET /order', function() {
+
+        echo '<h1>Form 1</h1>';
+    });
+    $f3->route('GET /order2', function() {
+
+        echo '<h1>Form 2</h1>';
+    });
+    $f3->route('GET /order2', function() {
+
+        echo '<h1>Form 2</h1>';
     });
 
     $f3->run();
