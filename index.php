112522aea04306d92a9f1a0f36b22d715cbc8b85
Dracologist <ejckanzler@yahoo.com>
2018-01-25 18:07:35 -0500

    fixed routing error in index.php

--- index.php
+++ index.php
@@ -35,6 +35,7 @@
         echo '<p>Thank you for ordering a(n) ' . $f3->get('color') . " " . $f3->get('animal') . "!</p>";
     });
     $f3->route('GET|POST /new-pet', function($f3) {
+        $view = new View;
         echo $view->render('views/form.html');
     });
 
