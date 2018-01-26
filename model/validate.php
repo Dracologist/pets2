14c6476e6d54bbb6a537f665b14167df71f351f2
Dracologist <ejckanzler@yahoo.com>
2018-01-26 17:28:43 -0500

    Added validString function

--- model/validate.php
+++ model/validate.php
@@ -1,9 +1,14 @@
+<?php
 /* Validate a color
  *
  * @param String color
  * @return boolean
  */
-function validColor() {
+function validColor($color) {
     global $f3;
-    return in_array($color, $f3-get('colors');
+    return in_array($color, $f3-get('colors'));
+}
+
+function validString($string){
+    return isset($string) && ctype_alpha($string);
 }
